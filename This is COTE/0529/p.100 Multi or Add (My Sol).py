d99f85a2b214c6427db0c9d2d8bbaf53ed0a7cd3
wonderkyeom <wonderkyum21@naver.com>
2022-05-29 16:59:07 +0900

    0529 Algorithm

--- This is COTE/0529/p.100 Multi or Add (My Sol).py
+++ This is COTE/0529/p.100 Multi or Add (My Sol).py
@@ -4,7 +4,7 @@ s = input()
 start = time.time()
 answer = 0
 for num in s:
-    if answer == 0:
+    if answer == 0: # 이건 틀린거임.. 1인 경우에도 더하는 것이 곱하는 것보다 큼!
         answer += int(num)
     else:
         answer *= int(num)
