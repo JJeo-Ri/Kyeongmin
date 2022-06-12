654530b010278ea6a4fabedafd51db972ef884f2
wonderkyeom <wonderkyum21@naver.com>
2022-06-12 11:57:48 +0900

    Algorithm 0612

--- This is COTE/0611/p.313 String Split (My Sol).py
+++ This is COTE/0611/p.313 String Split (My Sol).py
@@ -1,17 +1,34 @@
-# 0과 1의 갯수를 체크
-# 작은 것을 큰 갯수로 replace
+# 0과 1의 갯수를 체크 -> 작은 것을 큰 갯수로 replace
+import time
 
 s = input()
-count_z = s.count('0')
-count_o = s.count('1')
+start = time.time()
+s += 'x'
 answer = 0
 
-print(count_z, count_o)
-if count_z >= count_o:
-    print('1'*count_o)
-    s.replace('1'*count_o, '0'*count_o)
-else:
-    print('0'*count_z)
-    s.replace('0'*count_z, '1'*count_z)
+g = []
+count_0 = 0
+count_1 = 0
 
-print(s)
+while s != 'x':
+    for idx in range(len(s) - 1):
+        if s[idx] == s[idx + 1]:
+            # print(s)
+            continue
+        else:
+            if s[idx] == '0':
+                count_0 += 1
+            else:
+                count_1 += 1
+
+            g.append(s[:idx + 1])
+            # print(g)
+
+            s = s[idx + 1:]
+            # print(s)
+            break
+              
+print(count_0 if count_0 < count_1 else count_1)
+end = time.time()
+print("걸린 시간: ", end - start)
+# 걸린 시간:  8.7738037109375e-05
