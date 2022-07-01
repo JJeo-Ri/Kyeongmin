98dbbb7a88d10081716081146b98d4268ff703bb
wonderkyeom <wonderkyum21@naver.com>
2022-07-01 21:01:51 +0900

    0701 Algorithm

--- This is COTE/0701/p.149 Juice Freezing (My sol).py
+++ This is COTE/0701/p.149 Juice Freezing (My sol).py
@@ -1,11 +1,13 @@
+import time
+
 n, m = map(int, input().split())
 graph = []
 answer = 0
 
 for _ in range(n):
     graph.append(list(map(int, input())))
-
 # print(graph)
+start = time.time()
 
 def dfs(i, j):
     # 종료 조건 기입
@@ -20,10 +22,14 @@ def dfs(i, j):
     dfs(i - 1, j)
     dfs(i, j - 1)
 
-    answer += 1
+    return 1
 
 for i in range(n):
     for j in range(m):
-        dfs(i, j)
+        if dfs(i, j) == 1:
+            answer += 1
 
 print(answer)
+end = time.time()
+print("걸린 시간 :", end - start)
+# 걸린 시간 : 0.00030350685119628906
