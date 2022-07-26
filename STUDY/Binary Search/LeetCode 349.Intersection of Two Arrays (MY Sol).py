b82684bfc54450c3fc052afeec36e76e24b5dde4
wonderkyeom <wonderkyum21@naver.com>
2022-07-26 21:34:36 +0900

    0726 Algorithm

--- STUDY/Binary Search/LeetCode 349.Intersection of Two Arrays (MY Sol).py
+++ STUDY/Binary Search/LeetCode 349.Intersection of Two Arrays (MY Sol).py
@@ -1,3 +1,17 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:        
-        return nums.index(target) if target in nums else -1
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1, nums2 = set(nums1), set(nums2)
+        # print(nums1, nums2)
+        
+        # 무조건 작은 크기를 nums1으로..
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        answer = []
+        
+        nums1, nums2 = list(nums1), list(nums2)
+        for idx in range(len(nums1)):
+            if nums1[idx] in nums2:
+                answer.append(nums1[idx])
+                
+        return answer
