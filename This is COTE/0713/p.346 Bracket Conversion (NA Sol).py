b282b9b5b5461c6b9b7150903e1b7d9584a77b83
wonderkyeom <wonderkyum21@naver.com>
2022-08-18 21:31:41 +0900

    0818 Algorithm

--- This is COTE/0713/p.346 Bracket Conversion (NA Sol).py
+++ This is COTE/0713/p.346 Bracket Conversion (NA Sol).py
@@ -1,6 +1,8 @@
 # "균형잡힌 괄호 문자열"의 인덱스 반환
 def balanced_index(p):
-    count = 0 # 왼쪽 괄호의 갯수
+    # 왼쪽 괄호의 갯수
+    count = 0
+    
     for i in range(len(p)):
         if p[i] == '(':
             count += 1
@@ -11,16 +13,20 @@ def balanced_index(p):
 
 # "올바른 괄호 문자열"인지 판단
 def check_proper(p):
-    count = 0 # 왼쪽 괄호의 갯수
+    # 왼쪽 괄호의 갯수
+    count = 0
+    
     for i in p:
         if i == '(':
             count += 1
         else:
-            if count == 0: # 쌍이 맞지 않는 경우에 False 반환
+            # 쌍이 맞지 않는 경우에 False 반환
+            if count == 0: 
                 return False
             count -= 1
-
-    return True # 쌍이 맞는 경우에 True 반환
+            
+    # 쌍이 맞는 경우에 True 반환
+    return True 
 
 def solution(p):
     answer = ''
