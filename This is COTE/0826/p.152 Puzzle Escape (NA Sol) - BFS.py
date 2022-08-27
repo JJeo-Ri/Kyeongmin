8350a1a71f8a413118f3fa7c7770792488bf0379
wonderkyeom <wonderkyum21@naver.com>
2022-08-27 21:40:16 +0900

    0827 Algorithm

--- This is COTE/0826/p.152 Puzzle Escape (NA Sol) - BFS.py
+++ This is COTE/0826/p.152 Puzzle Escape (NA Sol) - BFS.py
@@ -5,23 +5,29 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int, input())))
 
+# 상, 하, 좌, 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs(i, j):
+    # deque로 큐 생성
     queue = deque()
     queue.append((i, j))
 
+    # 큐가 빌 때 까지 계속 수행!
     while queue:
         x, y = queue.popleft()
 
         for i in range(4):
+            # 다음 x, y 좌표 수행(dx, dy 값을 더해서)
             nx = x + dx[i]
             ny = y + dy[i]
 
+            # 수행할 수 없는 위치이면 PASS
             if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
 
+            # 처음 방문했을 때..
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
